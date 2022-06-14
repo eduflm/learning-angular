4f16ee62d5fb807171c60cd783e14a7db69f401c
Eduardo Lima <eduardo.fernandodelima@gmail.com>
2022-06-14 20:00:33 -0300

    ending with basic stuff

--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -10,21 +10,25 @@ export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreation = 'No server was created';
   serverName = '';
+  serverCreated = false;
+  servers = ['Server1', 'Server2']
 
   constructor() {
     setTimeout(() => {
       this.allowNewServer = true
     }, 1000)
-   }
+  }
 
   ngOnInit(): void {
   }
 
   onCreateServer() {
     this.serverCreation = 'Server was created! New server name: ' + this.serverName;
+    this.servers.push(this.serverName);
+    this.serverCreated = true
   }
 
-  onUpdateServerName(event : any) {
+  onUpdateServerName(event: any) {
     this.serverName = event.target.value
   }
 
