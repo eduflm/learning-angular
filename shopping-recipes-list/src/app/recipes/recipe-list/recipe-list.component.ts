612bd0cd37e081d284c33c18c4dd7020f8abb940
Eduardo Lima <eduardo.fernandodelima@gmail.com>
2022-06-20 16:02:40 -0300

    update recipes project

---
+++ shopping-recipes-list/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,28 @@
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+
+  recipes: Recipe[] = [
+    new Recipe('Teste Recipe', 'This is a test', 'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=556,505'),
+    new Recipe('Pizza', 'Essa é uma Pizza', 'https://www.simplyrecipes.com/thmb/JWjdE8YwikAae0KZuyy6ZJW7Utw=/3000x2001/filters:no_upscale():max_bytes(150000):strip_icc()/Simply-Recipes-Homemade-Pizza-Dough-Lead-Shot-1c-c2b1885d27d4481c9cfe6f6286a64342.jpg')
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onRecipeSelected(recipeSelected: Recipe) {
+    console.log("onRecipeSelected")
+    console.log(recipeSelected)
+    this.recipeWasSelected.emit(recipeSelected);
+  }
+
+}
